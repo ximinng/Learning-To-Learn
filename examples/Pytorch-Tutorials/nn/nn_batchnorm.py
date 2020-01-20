533641d81a5556b85347aada43ecc4207fdb5235
Ximing Xing <ximingxing@gmail.com>
2020-01-20 11:34:55 +0800

    update pytorch tutorials: batch norm

---
+++ examples/Pytorch-Tutorials/nn/nn_batchnorm.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+   Description :   BatchNorm
+                   1. Converge faster
+                   2. Better performance
+                   3. Robust : stable, larger learning rate
+   Author :        xxm
+"""

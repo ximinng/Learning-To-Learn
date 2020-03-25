1b5abd28bc64d40ec2ad8a3bfc6fdc4aaff7cef8
Ximing Xing <ximingxing@gmail.com>
2020-03-25 17:10:46 +0800

    fix code train in terminal

--- learningTolearn/__init__.py
+++ learningTolearn/__init__.py
@@ -18,4 +18,12 @@ path = {
 for k, v in path.items():
     sys.path.append(v)
 
-#
+# module not found:
+"""
+import sys
+import os
+
+curPath = os.path.abspath(os.path.dirname(__file__))
+rootPath = os.path.split(curPath)[0]
+sys.path.append(rootPath)
+"""
